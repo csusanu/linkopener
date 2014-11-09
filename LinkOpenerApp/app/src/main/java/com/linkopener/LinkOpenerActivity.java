7800e663cafd702b7f9a8f0d45fdb5a9b161b4f1
Stefan Ghiaus <sghiaus@gmail.com>
2014-11-09 16:52:03 +0200

    Removes settings activity. Changes link opener to return when the intent is not ACTION_VIEW.

--- LinkOpenerApp/app/src/main/java/com/linkopener/LinkOpenerActivity.java
+++ LinkOpenerApp/app/src/main/java/com/linkopener/LinkOpenerActivity.java
@@ -1,6 +1,7 @@
 package com.linkopener;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,6 +12,10 @@ public class LinkOpenerActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        if (intent.getAction() != Intent.ACTION_VIEW) {
+            finish();
+        }
     }
 
 }
