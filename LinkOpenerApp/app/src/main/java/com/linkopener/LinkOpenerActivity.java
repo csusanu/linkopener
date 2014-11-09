1f6762d3c2334b5ac046e800044e48e17d6180a7
C Susanu <csusanu@alumni.uwaterloo.ca>
2014-11-09 12:30:26 -0500

    Implementing main activity. It opens the file from the intent, reads the first line, then visits that URL.

--- LinkOpenerApp/app/src/main/java/com/linkopener/LinkOpenerActivity.java
+++ LinkOpenerApp/app/src/main/java/com/linkopener/LinkOpenerActivity.java
@@ -4,12 +4,19 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
 
-import java.net.URI;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
-
+/**
+ * Main LinkOpener activity. It reads the file from the intent and then opens the link inside it.
+ */
 public class LinkOpenerActivity extends Activity {
 
     @Override
@@ -20,8 +27,26 @@ public class LinkOpenerActivity extends Activity {
             finish();
         } else {
             Uri uri = intent.getData();
-            // TODO(csusanu):Do something with the URI.
+            try {
+                File file = new File(uri.getPath());
+                FileInputStream fileInputStream = new FileInputStream(file);
+                InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String firstLine = bufferedReader.readLine();
+                Log.i("linkopener", "Opening link: " + firstLine);
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(firstLine));
+                startActivity(browserIntent);
+            } catch (FileNotFoundException e) {
+                Log.e("linkopener", "Did not find file: " + uri.toString() + " due to: " + e);
+            } catch (IOException e) {
+                Log.e("linkopener", "Error reading file: " + uri.toString() + " due to: " + e);
+            }
         }
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        finish();
+    }
 }
