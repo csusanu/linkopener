f180183ee905a67c91fa435fe2717aa30beefeab
Stefan Ghiaus <sghiaus@gmail.com>
2014-11-09 16:54:52 +0200

    Adds TODO.

--- LinkOpenerApp/app/src/main/java/com/linkopener/LinkOpenerActivity.java
+++ LinkOpenerApp/app/src/main/java/com/linkopener/LinkOpenerActivity.java
@@ -2,9 +2,12 @@ package com.linkopener;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import java.net.URI;
 
 
 public class LinkOpenerActivity extends Activity {
@@ -15,6 +18,9 @@ public class LinkOpenerActivity extends Activity {
         Intent intent = getIntent();
         if (intent.getAction() != Intent.ACTION_VIEW) {
             finish();
+        } else {
+            Uri uri = intent.getData();
+            // TODO(csusanu):Do something with the URI.
         }
     }
 
